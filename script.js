a776fd50d2f4cadf140bf8947f3236ca777b9cdb
Kasia <kasia.bondarava@gmail.com>
2023-02-13 10:43:28 -0500

    Display user data fom API in UI

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const APIURL = "https://api.github.com/users/";
 
+const main = document.getElementById("main");
 const form = document.getElementById("form");
 const search = document.getElementById("search");
 
@@ -7,14 +8,46 @@ async function getUser(username) {
     try {
         const { data } = await axios(APIURL + username);
         console.log(data);
-        // createUserCard(data)
+        createUserCard(data);
         // getRepos(username)
     } catch(err) {
         if(err.response.status == 404) {
             console.log(err.response.data)
-            // createErrorCard("No profile with this username");
+            createErrorCard("No profile with this username");
         }
     }
+}
+
+function createUserCard(user) {
+    const cardHTML = `
+        <div class="card">
+            <div>
+                <img src=${user.avatar_url} alt="${user.name}" class="avatar">
+            </div>
+
+            <div class="user-info">
+                <h2>${user.name}</h2>
+                <p>${user.bio}</p>
+
+            <ul>
+                <li>${user.folowers}<strong>Followers</strong></li>
+                <li>${user.following}<strong>Following</strong></li>
+                <li>${user.public_repos}<strong>Repos</strong></li>
+            </ul>
+
+            <div id="repos"></div>
+        </div>
+    </div>
+    `;
+    main.innerHTML = cardHTML;
+}
+
+function createErrorCard(msg) {
+    const cardHTML = 
+        `<div class="card">
+            <h1>${msg}</h1>
+        </div>`;
+    main.innerHTML = cardHTML;
 }
 
 form.addEventListener("submit", e => {
